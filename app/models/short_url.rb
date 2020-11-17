11e17921b657573dc732f5f7a854cc482d35fdbc
ssjuampi23 <sspablo23@hotmail.com>
2020-11-17 12:42:36 -0600

    URL Model
    * I added the missing short_code column through a Rails migration with an index because this field will be queried as well
    
    * Use the UrlEncoder service inside the #short_code method in the ShortUrl model to be able to encode the URLs on the fly based on the current table ID
    
    * Use a Regex expression to be able to tell if the url of a ShortUrl model is valid or not. The Regex validates if the URL contains the following patterns at the beginning of the url: 'https', 'http', 'www', '/' or '//'.
    
    * Create a Custom Validator class 'Validators::FullUrlValidator' to extract the validation logic from the model
    
    * Add the after_create callback to be able to set the short_code value after the ID of the record is generated

--- app/models/short_url.rb
+++ app/models/short_url.rb
@@ -1,10 +1,10 @@
 class ShortUrl < ApplicationRecord
-
-  CHARACTERS = [*'0'..'9', *'a'..'z', *'A'..'Z'].freeze
-
-  validate :validate_full_url
+  validates :full_url, presence: true
+  validates_with Validators::FullUrlValidator
+  after_create :encode_short_code
 
   def short_code
+    UrlEncoder.new(self.id).call unless self.id.nil?
   end
 
   def update_title!
@@ -12,7 +12,8 @@ class ShortUrl < ApplicationRecord
 
   private
 
-  def validate_full_url
+  def encode_short_code
+    self.short_code = UrlEncoder.new(self.id).call
+    self.save
   end
-
 end
